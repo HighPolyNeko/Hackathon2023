8e0c3d29212c1646e52141e19f2428a4f26cb2f1
Nabokov Nikolay <n.nabokov1109@gmail.com>
2023-10-21 02:08:25 +0300

    WebApp

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import logging
 import sqlite3
-from settings.config import TG_TOKEN
-from aiogram import Bot, Dispatcher, types
+from settings.config import TG_TOKEN, WEB_APP_URL
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.types.web_app_info import WebAppInfo
 
 API_TOKEN = TG_TOKEN
 
@@ -9,43 +10,13 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
 
-# Подключение к базе данных "Death_Note"
-conn = sqlite3.connect('Death_Note.db')
-cursor = conn.cursor()
 
+@dp.message_handler(commands=['showWebApp'])
+async def show_web_app(message: types.Message):
+    markup = types.ReplyKeyboardMarkup()
+    markup.add(types.KeyboardButton('Открыть меню', web_app=WebAppInfo(url=WEB_APP_URL)))
+    await message.answer("ado", reply_markup=markup)
 
-# Создание таблицы для хранения пользователей (если она еще не создана)
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users (
-        telegram_id INTEGER PRIMARY KEY,
-        first_name TEXT,
-        last_name TEXT,
-        email TEXT,
-        password TEXT
-    )
-''')
-conn.commit()
-
-async def on_start(message: types.Message):
-    user_id = message.from_user.id
-    if user_id:
-        await message.answer(f"Привет, {user_id[1]}! Добро пожаловать!")
-    else:
-        await message.answer("Привет! Ты не зарегистрирован. Пожалуйста, зарегистрируйся.")
-
-
-# Приветственное сообщение
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await message.answer("Привет, пидор! Я могу помочь тебе с покупками в твоем обоссаном вагоне, если ты мне скажешь свой логин и пароль, а если у тебя его нет, так загистрируйся нахуй, а не морочь не мозги.")
-
-# Обработка всех остальных сообщений
-@dp.message_handler()
-async def echo_message(message: types.Message):
-    global Vremenniy_cringe
-    Vremenniy_cringe = message.text
-    print(Vremenniy_cringe)
-    #await message.answer(message.text)
 
 if __name__ == '__main__':
     from aiogram import executor
--- src/settings/config.py
+++ src/settings/config.py
@@ -5,12 +5,4 @@ import os
 load_dotenv()
 
 TG_TOKEN = os.environ.get("TG_TOKEN")
-DB_HOST = os.environ.get("DB_HOST")
-DB_PORT = os.environ.get("DB_PORT")
-DB_NAME = os.environ.get("DB_NAME")
-DB_PASS = os.environ.get("DB_PASS")
-
-SECRET_AUTH = os.environ.get("SECRET_AUTH")
-
-SERVER_HOST = str(os.environ.get("SERVER_HOST"))
-SERVER_PORT = os.environ.get("SERVER_PORT")
+WEB_APP_URL = os.environ.get("WEB_APP_URL")
