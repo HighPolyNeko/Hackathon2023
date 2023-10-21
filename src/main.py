a8029eea9892a81efa690ccbfa86fe7810ba89dd
Nabokov Nikolay <n.nabokov1109@gmail.com>
2023-10-21 11:19:02 +0300

    WebApp

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import logging
 import sqlite3
-from settings.config import TG_TOKEN, WEB_APP_URL, AUTH_URL
+from settings.config import TG_TOKEN, WEB_APP_URL, AUTH_URL, REG_URL
 import requests
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.types.web_app_info import WebAppInfo
@@ -11,29 +11,19 @@ bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
 
+@dp.message_handler(commands=['start'])
 async def on_start(message: types.Message):
     user_id = message.from_user.id
     r = requests.get(f'{AUTH_URL}')
-    r.status_code
+
+    markup = types.InlineKeyboardMarkup()
 
     if r.status_code == 200:
-        await message.answer(f"Привет, {user_id[1]}! Добро пожаловать!")
+        markup.add(types.InlineKeyboardButton('Открыть меню', web_app=WebAppInfo(url='/index.html')))
+        await message.answer(f"Привет, {message.from_user.full_name}", reply_markup=markup)
     else:
-        await message.answer("Привет! Ты не зарегистрирован. Пожалуйста, зарегистрируйся.")
-
-
-# Приветственное сообщение
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await message.answer("Привет, пидор! Я могу помочь тебе с покупками в твоем обоссаном вагоне, если ты мне скажешь свой логин и пароль, а если у тебя его нет, так загистрируйся нахуй, а не морочь не мозги.")
-
-# Обработка всех остальных сообщений
-@dp.message_handler()
-async def echo_message(message: types.Message):
-    global Vremenniy_cringe
-    Vremenniy_cringe = message.text
-    print(Vremenniy_cringe)
-    #await message.answer(message.text)
+        markup.add(types.InlineKeyboardButton('Открыть меню', web_app=WebAppInfo(url=REG_URL)))
+        await message.answer(f"Привет! Ты не зарегистрирован. Пожалуйста, зарегистрируйся., {message.from_user.full_name}", reply_markup=markup)
 
 
 @dp.message_handler(commands=['showWebApp'])
