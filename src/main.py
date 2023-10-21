fd748893d361587d56c7f03964c19e52f095bb6f
Nabokov Nikolay <n.nabokov1109@gmail.com>
2023-10-21 03:54:01 +0300

    WebApp

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import logging
 import sqlite3
-from settings.config import TG_TOKEN, WEB_APP_URL
+from settings.config import TG_TOKEN, WEB_APP_URL, AUTH_URL
+import requests
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.types.web_app_info import WebAppInfo
 
@@ -9,6 +10,30 @@ API_TOKEN = TG_TOKEN
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 logging.basicConfig(level=logging.INFO)
+
+async def on_start(message: types.Message):
+    user_id = message.from_user.id
+    r = requests.get(f'{AUTH_URL}')
+    r.status_code
+
+    if r.status_code == 200:
+        await message.answer(f"Привет, {user_id[1]}! Добро пожаловать!")
+    else:
+        await message.answer("Привет! Ты не зарегистрирован. Пожалуйста, зарегистрируйся.")
+
+
+# Приветственное сообщение
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.answer("Привет, пидор! Я могу помочь тебе с покупками в твоем обоссаном вагоне, если ты мне скажешь свой логин и пароль, а если у тебя его нет, так загистрируйся нахуй, а не морочь не мозги.")
+
+# Обработка всех остальных сообщений
+@dp.message_handler()
+async def echo_message(message: types.Message):
+    global Vremenniy_cringe
+    Vremenniy_cringe = message.text
+    print(Vremenniy_cringe)
+    #await message.answer(message.text)
 
 
 @dp.message_handler(commands=['showWebApp'])
--- src/settings/config.py
+++ src/settings/config.py
@@ -6,3 +6,4 @@ load_dotenv()
 
 TG_TOKEN = os.environ.get("TG_TOKEN")
 WEB_APP_URL = os.environ.get("WEB_APP_URL")
+AUTH_URL = os.environ.get("AUTH_URL")
